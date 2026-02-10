9e58f4db96fa993a3e797c96b336cd3de4518ab4
Venkata Sivaji Pilli <venkatasivajipilli@gmail.com>
2026-02-10 09:50:32 +0700

    name added

--- DemoWebApi/Controllers/WeatherForecastController.cs
+++ DemoWebApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DemoWebApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "SIVAJI"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
